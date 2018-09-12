8c31cdabbb8d7bc4b97ce2029c7d4648f484894c
Emilie Haedt <emilie_haedt@tsu.aw.org>
2018-09-12 13:46:56 -0700

    message

--- AboutMe.java
+++ AboutMe.java
@@ -22,8 +22,30 @@ public class AboutMe{
 
 public static void main(String[] args) {
 
-  AboutMe Katty = new AboutMe(); //constructor/organization method
-  Scanner kattyIn = new Scanner (System.in); //creation of scanner
+  AboutMe katty = new AboutMe(); //constructor/organization method
+  System.out.println("aBOuT kAtTy");//basic text
 
-}
+  Scanner kattyIn = new Scanner (System.in); //creation of scanner
+  String personIn = kattyIn.next();//tells scanner what to do
+
+  if(personIn.equals("birthday")){
+    System.out.println("Katty's birthday is " + katty.Birthday);
+    }
+  if(personIn.equals("pokemon")){
+    System.out.println("Katty's favorite pokemon is " + katty.Pokemon);
+    }
+  if(personIn.equals("room mate")){
+    System.out.println("Katty's room mate is " + katty.RoomMate);
+    }
+  if(personIn.equals("dorm group")){
+    System.out.println("Katty's dorm group is " + katty.DormGroup);
+    }
+  if(personIn.equals("pets")){
+    System.out.println(katty.Pets);
+    }
+  if(personIn.equals("patronus")){
+    System.out.println("Katty's patronus is " + katty.Patronus);
+    }
+
+  }
 }
