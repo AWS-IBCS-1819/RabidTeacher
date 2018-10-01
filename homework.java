8094c62f5a1e77030f79f2196c1de7491ac5deb4
Emilie Haedt <emilie_haedt@tsu.local>
2018-09-30 18:12:08 -0700

    homework for Oct 1st

---
+++ homework.java
@@ -0,0 +1,13 @@
+
+for(int i = x; i < y; i++);//question 2
+
+for(int i = 0; i <= 9; i++);//question 3 only the for loop not enough to actually run
+
+
+int i = 1;
+int y = 1;
+int number = x;
+  for (i = 1; i <= number; i++){
+    y = y * i;
+}/*
+System.out.println("the factorial of x is " + y);//question 5
