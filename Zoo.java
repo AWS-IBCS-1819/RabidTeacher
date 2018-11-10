04c39c3a8f6dc61b1c471ac51a16b045c6b91c12
Emilie Haedt <emilie_haedt@m018085.local>
2018-11-10 10:22:41 -0800

    Zoo

---
+++ Zoo.java
@@ -0,0 +1,32 @@
+import java.util.*;
+import java.io.*;
+
+public class Zoo {
+public static void main(String[] args) {
+
+  System.out.println("Please pick a region you would like to visit: ");
+  System.out.println("Kanto \n Johto \n Hoenn \n Sinnoh \n Unova \n Kalos \n Alola");
+  Scanner input = new Scanner(System.in);
+  if(result.equals ("Kanto")){
+
+  }
+  if(result.equals ("Johto")){
+
+  }
+  if(result.equals ("Hoenn")){
+
+  }
+  if(result.equals ("Sinnoh")){
+
+  }
+  if(result.equals ("Unova")){
+
+  }
+  if(result.equals ("Kalos")){
+
+  }
+  if(result.equals ("Alola")){
+
+  }
+}
+}
